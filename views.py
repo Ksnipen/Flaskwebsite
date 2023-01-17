98e175143afb7a8c707f62588119bf4323e115df
Kenneth <111025513+Ksnipen@users.noreplay.github.com>
2023-01-18 00:10:07 +0100

    noen bugs, men basis fungerer

--- views.py
+++ views.py
@@ -1,24 +1,21 @@
-from flask import Blueprint, render_template, request, jsonify, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, session
+from datascraper import webscraper
 
 views = Blueprint(__name__, "/")
+ek = 0
 
-@views.route("/")
+@views.route("/", methods = ['POST', 'GET'])
 def home():
-    return render_template("index.html", randnumb = get_random_number())
+    return render_template("index.html")
 
-@views.route("/profile")
-def profile():
-    return render_template("profile.html")
-
-@views.route("/json")
-def get_json():
-    return jsonify({'name': 'Kenneth', 'coolness': 10})
-
-@views.route("/data")
-def get_data():
-    data = request.json
-    return jsonify(data)
-
-@views.route("/home")
-def go_to_home():
-    return redirect(url_for("views.home"))
+@views.route("/result", methods = ['POST', 'GET'])
+def result():
+    if request.method == 'POST' and request.form.get('finnkode'):
+        if webscraper.check_valid_finncode(request.form.get('finnkode')):
+            ek = request.form.get('ek')
+            interest = request.form.get('rente').replace(',','.')
+            url = f"https://www.finn.no/realestate/homes/ad.html?finnkode={request.form.get('finnkode')}"
+            property_data = webscraper.get_essensial_numbers_from_property(webscraper.get_soup(url))
+            monthly_municipal_fees = int(int(property_data['municipal_fees'].replace(' ','')) / 12)
+            return render_template("result.html", property_data = property_data, monthly_municipal_fees = monthly_municipal_fees, capital = ek, interest = interest)
+    return redirect(url_for('views.home'))
